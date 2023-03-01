51a87220440a379e9d092bf387c6b505e1bef4ce
Pollyana <pollyanarocha416@gmail.com>
2023-03-01 09:01:22 -0400

    revendo if e elif e tentando criar um loop

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-print("Brasil", "ganhou", 5, "titulos mundiais", sep="-")
+""" print("Brasil", "ganhou", 5, "titulos mundiais", sep="-")
 
 print("çslkdjf", "çdf ", end="====")
+ """
+num_secreto = 42
+
+chut = int(input("Digite seu num: "))
+
+print("voce digitou: ", chut)
+  
