0059f59f69dae020897690553c296a45d8c0f553
Pollyana <pollyanarocha416@gmail.com>
2023-03-02 09:27:31 -0400

    formatacao de str

---
+++ organizarVal.py
@@ -0,0 +1,15 @@
+print("R$: {:f}".format(1.59))#ele vai recber float
+
+# depois do ponto eu quero 3 nums etc
+print("R$: {:.2f}".format(2))
+
+#se eu quero prencher com 0
+print("R$: {:010.0f}".format(45))#.1f define o q vem depois
+# do ponto
+# o 7 representa o total de caractere
+
+# pra int
+print("R$: {:03d}".format(2))
+
+#data
+print("Data {:02d}/{:02d}/{:04d}".format(2, 3, 2023))
