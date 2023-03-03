2f4295b8e8a048a7f004a13eae8248b59fd961b4
Pollyana <pollyanarocha416@gmail.com>
2023-03-03 18:35:34 -0400

    __str__ pra printar direto a conta completa

--- OOP pt1/conta.py
+++ OOP pt1/conta.py
@@ -16,13 +16,21 @@ class Conta:
     def sacar(self, valor):
         self.saldo -= valor
 
+    def __str__(self) -> str:
+        print(f"Conta titular: {self.titular}, conta: {self.numero}, saldo atual: {self.saldo}, seu limite: {self.limite}")
+
+
 
 # testando
-c = Conta(123, "Polly", 100, 1000)
+c = Conta(123, "Polly", 1000, 1000)
+c.__str__()
+c.depositar(300)
+c.extrato()
+""" 
 c.extrato()
 c.depositar(12)
 c.extrato()
 c.sacar(100)
 c.extrato()
 c.depositar(2000)
-c.extrato()
+c.extrato() """
