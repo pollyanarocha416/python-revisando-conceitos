a5cef234de3e4ccc3b5c01540c3686e4bfb800bd
Pollyana <pollyanarocha416@gmail.com>
2023-03-07 11:16:50 -0400

    tratamento de de erros na palavras

--- jogos/forca.py
+++ jogos/forca.py
@@ -6,23 +6,29 @@ def jogo_forca():
     print("*********************************************")
     print("fim do jogo")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
-
+    erros = 0
 
     while(not enforcou and not acertou):
         
         chute = str(input("Qual letra? "))
-        chute = chute.strip()
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-                print(f"encontrei a letra [{letra}] na posicao {index}")
-            index = index +1
+        chute = chute.strip().upper()
+        
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                    print(f"encontrei a letra [{letra}] na posicao {index}")
+                index+=1
+        else:
+            erros+=1
+        #erros igual a 6
+        enforcou = erros == 6
         print(letras_acertadas)
 #execulta quando chamo diretamente
 if __name__ == ("__main__"):
