64a2a8a40df65c55fd145dc6ba49d77b0aa5a0aa
Pollyana <pollyanarocha416@gmail.com>
2023-03-15 11:31:28 -0400

    debugado e funcionando

--- curso-extrairURL/extrairurl.py
+++ curso-extrairURL/extrairurl.py
@@ -1,21 +1,27 @@
 #string e url
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanatiza_url(url)
         self.valida_url()
+    
     def sanatiza_url(self, url):
         return url.strip()
+    
     def valida_url(self):
-        if self.url == "":
+        if not self.url:
             raise ValueError("A URL está vazia")
+    
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
         url_base = url[:indice_interrogacao]
         return url_base
+    
     def get_url_parametros(self):
         indice_interrogacao = self.url.find('?')
         url_parametros = self.url[indice_interrogacao+1:]
         return url_parametros
+    
     def get_valor_parametro(self, parametro_busca):
         indice_parametro = self.get_url_parametros().find(parametro_busca)
         indice_valor = indice_parametro + len(parametro_busca) + 1
@@ -25,6 +31,7 @@ class ExtratorURL:
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
+    
 extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
