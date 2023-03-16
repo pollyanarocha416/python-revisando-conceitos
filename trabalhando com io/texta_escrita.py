e7b82a6665795b23f5ba089dd15ee148fc44bbac
Pollyana <pollyanarocha416@gmail.com>
2023-03-16 11:00:58 -0400

    usando o flush pra escrever no arquivo

--- trabalhando com io/texta_escrita.py
+++ trabalhando com io/texta_escrita.py
@@ -1,6 +1,14 @@
 arquivo_contatos = open(
-    'trabalhando com io/dados/contatos-escrita.csv', encoding='latin_1', mode='a')
+    'trabalhando com io/dados/contatos-escrita.csv', encoding='latin_1', mode='w')
 
-contato = '11,Carol,carol@carol.com.br\n'
+contatos = ['11,Carol,carol@carol.com.br\n',
+            '12,Letícia,leticia@leticia.com.br\n',
+            '13, Giulia,giulia@giulia.com.br\n',
+            '15, Felipe,felipe@felipe.com.br\n']
 
-arquivo_contatos.write(contato)
+
+for contato in contatos:
+    arquivo_contatos.write(contato)
+
+arquivo_contatos.flush()
+input('Pressione <Enter> para encerrar o programa')
