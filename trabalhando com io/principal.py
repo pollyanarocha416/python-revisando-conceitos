b8d75b436767f8c9db4b88d963bcfb175bc8d21f
Pollyana <pollyanarocha416@gmail.com>
2023-03-16 14:28:09 -0400

    mais sobre execoes

--- trabalhando com io/principal.py
+++ trabalhando com io/principal.py
@@ -1,7 +1,10 @@
 try:
-    arquivo_contatos = open('trabalhando com io/dados/contatos.csv', encoding='latin_1')
+    with open('trabalhando com io/dados/contatos.csv', encoding='latin_1') as arquivo_contatos:
 
-    for linha in arquivo_contatos:
-        print(linha, end='')
-finally:
-    arquivo_contatos.close()
+        for linha in arquivo_contatos:
+            print(linha, end='')
+except FileNotFoundError:
+    print('Arquivo nao encontrado')
+except PermissionError:
+    print('Sem permicao de escrita')
+
--- trabalhando com io/testa_concorrencia.py
+++ trabalhando com io/testa_concorrencia.py
@@ -1,12 +1,8 @@
-arquivos1 = open('trabalhando com io/dados/contatos-escrita.csv', encoding='latin_1', mode='w')
-arquivos2 = open('trabalhando com io/dados/contatos-escrita.csv',
-                 encoding='latin_1', mode='a')
 
 contato_carol = '11,Carol,carol@carol.com.br\n'
 contato_andreza = '12,Adreza,andreza@andreza.com.br\n'
 
-arquivos1.write(contato_carol)
-arquivos2.write(contato_andreza)
-
-arquivos1.close()
-arquivos2.close()
+with open('trabalhando com io/dados/contatos-escrita.csv', encoding='latin_1', mode='w') as arquivos1:
+    arquivos1.write(contato_carol)
+with open('trabalhando com io/dados/contatos-escrita.csv', encoding='latin_1', mode='a') as arquivos2:
+    arquivos2.write(contato_andreza)
