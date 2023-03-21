53c90fe9c160182d79dc70891ba8565392b54b23
Pollyana <pollyanarocha416@gmail.com>
2023-03-21 13:44:16 -0400

    retornando a estatistica resumida e detalhada

--- melhorando-o-codigo/main.py
+++ melhorando-o-codigo/main.py
@@ -1,6 +1,9 @@
 # from fila_normal import fila_normal
 """ from fila_prioritaria import FilaPrioritaria """
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
 
 """ fila_teste = fila_normal()
 fila_teste.atualizafila()
@@ -17,8 +20,10 @@ print(fila_teste_2.chama_cliente(20))
 print(fila_teste_2.chama_cliente(2))
 print(fila_teste_2.estatistica('10/01/1993', 12, 'detail'))
  """
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(2))
+print(teste_fabrica.estatistica('21/03/2023', 120, EstatisticaResumida))
+print(teste_fabrica.estatistica('21/03/2023', 120, EstatisticaDetalhada))
